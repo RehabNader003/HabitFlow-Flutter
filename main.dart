d262dd6c5c1dea95ca0ce41e2d7b3d225ec16a9b
Rehab Nader <167696223+RehabNader003@users.noreply.github.com>
2024-10-19 14:03:07 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,66 +1,21 @@
-// ignore_for_file: avoid_print
-
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:habit_project/create_new_habit.dart';
-import 'package:habit_project/notification.dart';
+import 'package:habitflow_depi/splashScreen/splash-screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
 
-  NotificationHandler.init();
-  runApp(const MyApp());
+void main() {
+  runApp(HabitlyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-    myRequestPermission();
-    getToken();
-  }
-
-  Future<void> myRequestPermission() async {
-    FirebaseMessaging messaging = FirebaseMessaging.instance;
-
-    NotificationSettings settings = await messaging.requestPermission(
-      alert: true,
-      announcement: false,
-      badge: true,
-      carPlay: false,
-      criticalAlert: false,
-      provisional: false,
-      sound: true,
-    );
-
-    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
-      print('User granted permission');
-    } else if (settings.authorizationStatus ==
-        AuthorizationStatus.provisional) {
-      print('User granted provisional permission');
-    } else {
-      print('User declined or has not accepted permission');
-    }
-  }
-
-  Future<void> getToken() async {
-    String? myToken = await FirebaseMessaging.instance.getToken();
-    print("FCM Token: $myToken");
-  }
-
+class HabitlyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: CreateHabitScreen(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'HabitFlow',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: SplashScreen(),  // Start with the splash screen
     );
   }
 }
---
+++ splash-screen.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import '../onboarding/onboarding-screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Navigate to Profile Page after 3 seconds
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => OnboardingScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFF8985E9),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 90,
+              backgroundColor: Color(0xFFFFFFFF),
+              child: CircleAvatar(
+                radius: 88,
+                backgroundImage:AssetImage("assets/images/Creativity.gif") ,
+              ),
+            ),
+            SizedBox(height: 25),
+            Text(
+              'HabitFlow',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 25),
+          SpinKitFadingCircle( // Loading spinner
+              color: Colors.white,
+              size: 50.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
